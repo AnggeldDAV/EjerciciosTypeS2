b91add7786019acf3ce3e0d7a0b606468407e8d2
AnggeldDAV <anggeld.adav@gmail.com>
2024-04-13 09:40:57 +0200

    funciones creadas

---
+++ EjerciciosTypeS2/ejercicioclases7.js
@@ -0,0 +1,94 @@
+"use strict";
+class Bicho {
+    constructor(nombre, edad, peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+    }
+}
+class OrdenaPorNombre {
+    ordena(BichoA, BichoB) {
+        return BichoA.nombre.localeCompare(BichoB.nombre);
+    }
+}
+class MuestraTipo01 {
+    muestraBicho(BichoA) {
+        console.log(`${BichoA.nombre} con una edad de ${BichoA.edad}, pesa 
+${BichoA.peso}`);
+    }
+}
+class Jaula {
+    constructor(Ordenacion, Mostrador) {
+        this.bichos = [];
+        this.Ordenacion = Ordenacion;
+        this.Mostrador = Mostrador;
+    }
+    add(BichoAPoner) {
+        this.bichos.push(BichoAPoner);
+    }
+    pesoTotal() {
+        let total = 0;
+        for (let bicho of this.bichos) {
+            total += bicho.peso;
+        }
+        return total;
+    }
+    edadTotal() {
+        let total = 0;
+        for (let bicho of this.bichos) {
+            total += bicho.edad;
+        }
+        return total;
+    }
+    edadMedia() {
+        let edadTotal = this.edadTotal();
+        return edadTotal / this.bichos.length;
+    }
+    ordena() {
+        this.bichos.sort(this.Ordenacion.ordena);
+    }
+    muestra() {
+        this.bichos.forEach(this.Mostrador.muestraBicho);
+    }
+}
+let Elefante = new Bicho("Jumbo", 45, 898);
+let Rinoceronte = new Bicho("Willie", 1, 1);
+let Koala = new Bicho("Kolinger", 56, 67);
+let Ordenacion = new OrdenaPorNombre();
+let Mostrado = new MuestraTipo01();
+let MiJaulaABC = new Jaula(Ordenacion, Mostrado);
+MiJaulaABC.add(Elefante);
+MiJaulaABC.add(Rinoceronte);
+MiJaulaABC.add(Koala);
+MiJaulaABC.muestra();
+MiJaulaABC.ordena();
+MiJaulaABC.muestra();
+//Que se quiere:
+//� Queremos ahora ordenar nuestra colecci�n por la edad de m�s joven a m�s viejo.
+class OrdenaPorEdad {
+    ordena(BichoA, BichoB) {
+        return BichoA.edad + BichoB.edad;
+    }
+}
+Ordenacion = new OrdenaPorEdad();
+MiJaulaABC.ordena();
+MiJaulaABC.muestra();
+//� Tambi�n queremos ordenarlos por peso, de m�s flaco a m�s gordo.
+class OrdenaPorPeso {
+    ordena(BichoA, BichoB) {
+        return BichoA.peso + BichoB.peso;
+    }
+}
+Ordenacion = new OrdenaPorPeso();
+MiJaulaABC.ordena();
+MiJaulaABC.muestra();
+//� Queremos mostrarlo ahora de forma distinta(el animalito { jumbo } tiene una edad
+//de { edad } y un { peso } estimado en peso
+class MuestraTipo02 {
+    muestraBicho(BichoA) {
+        console.log(`el animalito ${BichoA.nombre} tiene una edad de ${BichoA.edad} y un ${BichoA.peso} estimado en peso`);
+    }
+}
+Mostrado = new MuestraTipo02();
+MiJaulaABC.muestra();
+//# sourceMappingURL=ejercicioclases7.js.map
